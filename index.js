38a5aeaf990d5d113a5197c6b89d25e874ee9126
sohyeong-dev <so-hyeong@naver.com>
2020-09-15 09:14:22 +0000

    Create index.js (graphql-yoga quickstart)

---
+++ index.js
@@ -0,0 +1,23 @@
+import { GraphQLServer } from 'graphql-yoga'
+
+const typeDefs = `
+  type Query {
+    hello(name: String): String!
+  }
+`
+
+const resolvers = {
+  Query: {
+    hello: (_, { name }) => `Hello ${name || 'World'}`,
+  },
+}
+
+const options = {
+  port: 3000,
+  endpoint: '/graphql',
+  subscriptions: '/subscriptions',
+  playground: '/playground',
+}
+
+const server = new GraphQLServer({ typeDefs, resolvers })
+server.start(options, ({ port }) => console.log(`Server is running on learn-express.run.goorm.io:${port}`))
