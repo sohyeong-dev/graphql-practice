f5c18b5c1a76212f1ff7ee9a8e25d3698242d1fa
sohyeong-dev <so-hyeong@naver.com>
2020-09-17 07:57:45 +0000

    join

--- src/db/user.js
+++ src/db/user.js
@@ -1,11 +1,23 @@
 import { User } from '../../models'
 import bcrypt from 'bcryptjs'
 
+const saltRounds = 10
+
 export const getUsers = () => {
 	return User.findAll({}).then(users => users).catch(err => console.error(err))
 }
 
 export const getUserById = async id => {
 	const user = await User.findByPk(id)
+	return user
+}
+
+export const insertUser = async (name, pwd) => {
+	const salt = bcrypt.genSaltSync(saltRounds)
+	const hashedPassword = await bcrypt.hashSync(pwd, salt)
+	const user = await User.create({
+		name,
+		pwd: hashedPassword,
+	})
 	return user
 }
